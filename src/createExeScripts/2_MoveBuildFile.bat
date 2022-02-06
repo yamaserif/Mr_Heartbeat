78fd0b65059eb110874dd5ce12a8e330f5904249
yamaserif <62947068+yamaserif@users.noreply.github.com>
2022-02-06 18:24:29 +0900

    アプリに使用したライブラリのLicence表記を含める

--- src/createExeScripts/2_MoveBuildFile.bat
+++ src/createExeScripts/2_MoveBuildFile.bat
@@ -4,3 +4,6 @@ move MrHeartBeat.exe ..\..\exe
 echo D | xcopy /e controllers ..\..\exe\controllers
 echo D | xcopy /e views ..\..\exe\views
 echo D | xcopy /e customViews ..\..\exe\customViews
+echo D | xcopy /e licences ..\..\exe\licences
+
+powershell compress-archive ..\..\exe\* ..\..\app.zip
